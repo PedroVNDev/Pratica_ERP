cf14abb40e8ac6660566b867fe72bf87ff794b53
janovm <jano3708@hotmail.com>
2020-11-12 19:20:14 +0100

    v2

--- Practica_ERP/src/controlador/Login.java
+++ Practica_ERP/src/controlador/Login.java
@@ -6,10 +6,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
 
 public class Login extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -32,13 +38,50 @@ public class Login extends JFrame {
 	 */
 	public Login() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 824, 560);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.BLUE);
+		panel.setBounds(0, 0, 66, 523);
+		contentPane.add(panel);
 		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.BLUE);
+		panel_1.setBounds(48, 0, 772, 58);
+		contentPane.add(panel_1);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(Color.BLUE);
+		panel_2.setBounds(738, 36, 82, 487);
+		contentPane.add(panel_2);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(Color.BLUE);
+		panel_3.setBounds(10, 465, 790, 58);
+		contentPane.add(panel_3);
+		
+		JLabel lblNewLabel = new JLabel("Usuario:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel.setBounds(261, 272, 108, 63);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblContrase = new JLabel("Contrase\u00F1a:");
+		lblContrase.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblContrase.setBounds(235, 332, 108, 63);
+		contentPane.add(lblContrase);
+		
+		textField = new JTextField();
+		textField.setBounds(363, 297, 96, 19);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(363, 357, 96, 19);
+		contentPane.add(textField_1);
 	}
-
 }
