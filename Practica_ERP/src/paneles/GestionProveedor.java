25218a8f0e06d9bb11cb5e05a23c04c05eb377c3
PinguExtremo <pedrovicentenavas@gmail.com>
2020-11-30 18:16:40 +0100

    Gestion Contacto

--- Practica_ERP/src/paneles/GestionProveedor.java
+++ Practica_ERP/src/paneles/GestionProveedor.java
@@ -28,9 +28,12 @@ import java.io.IOException;
 import java.awt.event.ActionEvent;
 
 public class GestionProveedor extends JPanel {
+
+	private JTextField txtDni;
 	private JTextField txtNombre;
-	private JTextField txtCif;
-	private JTextField txtTipoProveedor;
+	private JTextField txtApellidos;
+	private JTextField txtID_Vehiculo;
+	private JTextField txtPrecio_Compra;
 	private JTextField txtCP;
 	private JTextField txtProvincia;
 	private JTextField txtPoblacion;
@@ -51,34 +54,46 @@ public class GestionProveedor extends JPanel {
 		setLayout(null);
 
 		// Labels Añadir
-		JLabel lblAniadirProveedor = new JLabel("A\u00F1adir Proveedor");
-		lblAniadirProveedor.setForeground(SystemColor.textHighlight);
-		lblAniadirProveedor.setFont(new Font("Tahoma", Font.BOLD, 24));
-		lblAniadirProveedor.setBounds(75, 10, 328, 40);
-		add(lblAniadirProveedor);
+		JLabel lblAniadirContacto = new JLabel("A\u00F1adir Contacto");
+		lblAniadirContacto.setForeground(SystemColor.textHighlight);
+		lblAniadirContacto.setFont(new Font("Tahoma", Font.BOLD, 24));
+		lblAniadirContacto.setBounds(75, 10, 328, 40);
+		add(lblAniadirContacto);
 
-		JLabel lblNombre = new JLabel("Nombre Proveedor:");
+		JLabel lblDNI = new JLabel("DNI Contacto: ");
+		lblDNI.setForeground(SystemColor.textHighlight);
+		lblDNI.setFont(new Font("Arial", Font.BOLD, 16));
+		lblDNI.setBounds(43, 54, 139, 15);
+		add(lblDNI);
+
+		JLabel lblNombre = new JLabel("Nombre Contacto:");
 		lblNombre.setForeground(SystemColor.textHighlight);
 		lblNombre.setFont(new Font("Arial", Font.BOLD, 16));
-		lblNombre.setBounds(10, 64, 149, 35);
+		lblNombre.setBounds(10, 74, 172, 35);
 		add(lblNombre);
 
-		JLabel lblTelefono = new JLabel("Tipo Proveedor:");
-		lblTelefono.setForeground(SystemColor.textHighlight);
-		lblTelefono.setFont(new Font("Arial", Font.BOLD, 16));
-		lblTelefono.setBounds(36, 150, 123, 14);
-		add(lblTelefono);
+		JLabel lblApellidos = new JLabel("Apellidos Contacto:");
+		lblApellidos.setForeground(SystemColor.textHighlight);
+		lblApellidos.setFont(new Font("Arial", Font.BOLD, 16));
+		lblApellidos.setBounds(10, 105, 172, 35);
+		add(lblApellidos);
 
-		JLabel lblCIF = new JLabel("CIF Proveedor: ");
-		lblCIF.setForeground(SystemColor.textHighlight);
-		lblCIF.setFont(new Font("Arial", Font.BOLD, 16));
-		lblCIF.setBounds(45, 109, 139, 15);
-		add(lblCIF);
+		JLabel lblIdVehiculo = new JLabel("ID Vehiculo:");
+		lblIdVehiculo.setForeground(SystemColor.textHighlight);
+		lblIdVehiculo.setFont(new Font("Arial", Font.BOLD, 16));
+		lblIdVehiculo.setBounds(63, 151, 96, 14);
+		add(lblIdVehiculo);
+
+		JLabel lblPrecio_Compra = new JLabel("Precio_Compra:");
+		lblPrecio_Compra.setForeground(SystemColor.textHighlight);
+		lblPrecio_Compra.setFont(new Font("Arial", Font.BOLD, 16));
+		lblPrecio_Compra.setBounds(22, 181, 131, 14);
+		add(lblPrecio_Compra);
 
 		JLabel lblCP = new JLabel("CP:");
 		lblCP.setForeground(SystemColor.textHighlight);
 		lblCP.setFont(new Font("Arial", Font.BOLD, 16));
-		lblCP.setBounds(114, 192, 66, 14);
+		lblCP.setBounds(104, 206, 66, 14);
 		add(lblCP);
 
 		JLabel lblProvincia = new JLabel("Provincia:");
@@ -96,7 +111,7 @@ public class GestionProveedor extends JPanel {
 		JLabel lblCalle = new JLabel("Calle:");
 		lblCalle.setForeground(SystemColor.textHighlight);
 		lblCalle.setFont(new Font("Arial", Font.BOLD, 16));
-		lblCalle.setBounds(104, 311, 80, 16);
+		lblCalle.setBounds(104, 312, 55, 16);
 		add(lblCalle);
 
 		// Labels en medio
@@ -112,48 +127,61 @@ public class GestionProveedor extends JPanel {
 		lblSotecars2.setBounds(587, 129, 152, 22);
 		add(lblSotecars2);
 
-		JLabel lblListaClientes = new JLabel("Lista de clientes");
+		JLabel lblListaClientes = new JLabel("Lista de Contactos");
 		lblListaClientes.setForeground(SystemColor.textHighlight);
 		lblListaClientes.setFont(new Font("Tahoma", Font.BOLD, 22));
 		lblListaClientes.setBounds(566, 346, 214, 40);
 		add(lblListaClientes);
 
 		// Labels Eliminar
-		JLabel lblEliminarProveedor = new JLabel("Eliminar Proveedor");
+		JLabel lblEliminarProveedor = new JLabel("Eliminar Contacto");
 		lblEliminarProveedor.setForeground(SystemColor.textHighlight);
 		lblEliminarProveedor.setFont(new Font("Tahoma", Font.BOLD, 24));
 		lblEliminarProveedor.setBounds(972, 10, 265, 40);
 		add(lblEliminarProveedor);
 
-		JLabel lblIdEliminar = new JLabel("ID Proveedor:");
+		JLabel lblIdEliminar = new JLabel("ID Contacto:");
 		lblIdEliminar.setForeground(SystemColor.textHighlight);
 		lblIdEliminar.setFont(new Font("Arial", Font.BOLD, 16));
 		lblIdEliminar.setBounds(972, 186, 139, 20);
 		add(lblIdEliminar);
 
 		// JTextFields Añadir
+
+		txtDni = new JTextField();
+		txtDni.setBackground(SystemColor.inactiveCaption);
+		txtDni.setColumns(10);
+		txtDni.setBounds(171, 53, 121, 20);
+		add(txtDni);
+
 		txtNombre = new JTextField();
 		txtNombre.setBackground(SystemColor.inactiveCaption);
-		txtNombre.setBounds(171, 74, 121, 20);
+		txtNombre.setBounds(171, 84, 121, 20);
 		add(txtNombre);
 		txtNombre.setColumns(10);
 
-		txtCif = new JTextField();
-		txtCif.setBackground(SystemColor.inactiveCaption);
-		txtCif.setColumns(10);
-		txtCif.setBounds(171, 108, 123, 20);
-		add(txtCif);
+		txtApellidos = new JTextField();
+		txtApellidos.setColumns(10);
+		txtApellidos.setBackground(SystemColor.inactiveCaption);
+		txtApellidos.setBounds(171, 114, 121, 20);
+		add(txtApellidos);
 
-		txtTipoProveedor = new JTextField();
-		txtTipoProveedor.setColumns(10);
-		txtTipoProveedor.setBackground(SystemColor.inactiveCaption);
-		txtTipoProveedor.setBounds(169, 150, 123, 20);
-		add(txtTipoProveedor);
+		txtID_Vehiculo = new JTextField();
+		txtID_Vehiculo.setColumns(10);
+		txtID_Vehiculo.setBackground(SystemColor.inactiveCaption);
+		txtID_Vehiculo.setBounds(169, 150, 123, 20);
+		add(txtID_Vehiculo);
+
+		txtPrecio_Compra = new JTextField();
+		txtPrecio_Compra.setColumns(10);
+		txtPrecio_Compra.setBackground(SystemColor.inactiveCaption);
+		txtPrecio_Compra.setBounds(169, 176, 123, 20);
+		add(txtPrecio_Compra);
 
 		txtCP = new JTextField();
 		txtCP.setColumns(10);
 		txtCP.setBackground(SystemColor.inactiveCaption);
-		txtCP.setBounds(169, 192, 123, 20);
+		txtCP.setBounds(169, 200, 123, 20);
 		add(txtCP);
 
 		txtProvincia = new JTextField();
@@ -182,7 +210,7 @@ public class GestionProveedor extends JPanel {
 		add(txtIdEliminar);
 
 		// Botones
-		JButton btnAniadir = new JButton("A\u00F1adir Proveedor");
+		JButton btnAniadir = new JButton("A\u00F1adir Contacto");
 		btnAniadir.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 
@@ -199,7 +227,7 @@ public class GestionProveedor extends JPanel {
 		btnAniadir.setBounds(104, 360, 214, 35);
 		add(btnAniadir);
 
-		JButton btnEliminar = new JButton("Eliminar Proveedor");
+		JButton btnEliminar = new JButton("Eliminar Contacto");
 		btnEliminar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 
@@ -228,8 +256,8 @@ public class GestionProveedor extends JPanel {
 		table = new JTable();
 		scrollPane.setViewportView(table);
 
-		modeloTabla.setColumnIdentifiers(
-				new Object[] { "ID", "CIF", "Nombre", "Tipo_Proveedor", "CP", "Poblacion", "Provincia", "Calle" });
+		modeloTabla.setColumnIdentifiers(new Object[] { "ID", "DNI", "Nombre", "Apellidos", "ID_Vehiculo",
+				"Precio_Compra", "CP", "Poblacion", "Provincia", "Calle" });
 		table.setModel(modeloTabla);
 
 		modeloTabla.setRowCount(0);
@@ -247,12 +275,13 @@ public class GestionProveedor extends JPanel {
 				conexion = DriverManager.getConnection("jdbc:mysql://localhost/SotecarsBBDD", "TRABAJO", "TRABAJO");
 				sql = conexion.createStatement();
 				rs = sql.executeQuery(
-						"SELECT ID, CIF, Nombre, Tipo_Proveedor, CP, Provincia, Poblacion, Calle FROM Proveedores");
+						"SELECT ID, DNI, Nombre, Apellidos, ID_Vehiculo, Precio_Compra, CP, Provincia, Poblacion, Calle FROM contactos_compra");
 
 				while (rs.next()) {
-					modeloTabla.addRow(new Object[] { rs.getObject("ID"), rs.getObject("CIF"), rs.getObject("Nombre"),
-							rs.getObject("Tipo_Proveedor"), rs.getObject("CP"), rs.getObject("Provincia"),
-							rs.getObject("Poblacion"), rs.getObject("Calle") });
+					modeloTabla.addRow(new Object[] { rs.getObject("ID"), rs.getObject("DNI"), rs.getObject("Nombre"),
+							rs.getObject("Apellidos"), rs.getObject("ID_Vehiculo"), rs.getObject("Precio_Compra"),
+							rs.getObject("CP"), rs.getObject("Provincia"), rs.getObject("Poblacion"),
+							rs.getObject("Calle") });
 				}
 
 				conexion.close();
@@ -268,17 +297,19 @@ public class GestionProveedor extends JPanel {
 
 		try {
 
+			String dni = txtDni.getText();
 			String nombre = txtNombre.getText();
-			String tipoproveedor = txtTipoProveedor.getText();
-			String cif = txtCif.getText();
+			String apellidos = txtID_Vehiculo.getText();
+			int idvehiculo = Integer.parseInt(txtID_Vehiculo.getText());
+			int precio_compra = Integer.parseInt(txtPrecio_Compra.getText());
 			int cp = Integer.parseInt(txtCP.getText());
 			String provincia = txtProvincia.getText();
 			String poblacion = txtPoblacion.getText();
 			String calle = txtCalle.getText();
 
-			String agregar = "INSERT INTO proveedores (CIF, Nombre, Tipo_Proveedor, CP, Provincia, Poblacion, Calle) VALUES('"
-					+ nombre + "', '" + tipoproveedor + "', '" + cif + "', '" + cp + "', '" + provincia + "', '"
-					+ poblacion + "', '" + calle + "')";
+			String agregar = "INSERT INTO contacto_compra (DNI, Nombre, Apellidos, ID_Vehiculo, Precio_Compra, CP, Provincia, Poblacion, Calle) VALUES('"
+					+ dni + "', '" + nombre + "', '" + apellidos + "', '" + idvehiculo + "', '" + precio_compra + "', '"
+					+ cp + "', '" + provincia + "', '" + poblacion + "', '" + calle + "')";
 			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/SotecarsBBDD", "TRABAJO",
 					"TRABAJO");
 
@@ -286,7 +317,7 @@ public class GestionProveedor extends JPanel {
 
 			consulta.executeUpdate(agregar);
 
-			JOptionPane.showMessageDialog(null, "Proveedor Agregado Correctamente");
+			JOptionPane.showMessageDialog(null, "Contacto Agregado Correctamente");
 
 			conexion.close();
 
@@ -296,22 +327,22 @@ public class GestionProveedor extends JPanel {
 	}
 
 	public void eliminarCliente() {
-		int confirmar = JOptionPane.showConfirmDialog(null, "¿Quieres eliminar el cliente escrito?");
+		int confirmar = JOptionPane.showConfirmDialog(null, "¿Quieres eliminar el contacto escrito?");
 
 		if (confirmar == JOptionPane.YES_OPTION) {
 
 			try {
 
-				String proveedor = (txtIdEliminar.getText());
+				String contacto = (txtIdEliminar.getText());
 
-				String eliminar = "DELETE FROM proveedores WHERE ID = '" + proveedor + "'";
+				String eliminar = "DELETE FROM contactos_compra WHERE ID = '" + contacto + "'";
 				Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/SotecarsBBDD", "TRABAJO",
 						"TRABAJO");
 				Statement consulta = conexion.createStatement();
 
 				consulta.executeUpdate(eliminar);
 
-				JOptionPane.showMessageDialog(null, "Proveedor Eliminado Correctamente");
+				JOptionPane.showMessageDialog(null, "Contacto Eliminado Correctamente");
 
 				conexion.close();
 			} catch (Exception ex2) {
@@ -320,15 +351,17 @@ public class GestionProveedor extends JPanel {
 			}
 
 		} else {
-			JOptionPane.showMessageDialog(null, "No se eliminara el proveedor introducido");
+			JOptionPane.showMessageDialog(null, "No se eliminara el contacto introducido");
 		}
 	}
 
 	public void resetTextfieldsGestionProveedor() {
 
+		txtDni.setText("");
 		txtNombre.setText("");
-		txtCif.setText("");
-		txtTipoProveedor.setText("");
+		txtApellidos.setText("");
+		txtID_Vehiculo.setText("");
+		txtPrecio_Compra.setText("");
 		txtCP.setText("");
 		txtProvincia.setText("");
 		txtPoblacion.setText("");
