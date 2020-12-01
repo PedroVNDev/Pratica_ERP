c6a159246e938261a59a031cb6c6e17ce16965af
PinguExtremo <pedrovicentenavas@gmail.com>
2020-12-01 17:33:00 +0100

    Gestionar Venta

--- Practica_ERP/src/paneles/GestionarVenta.java
+++ Practica_ERP/src/paneles/GestionarVenta.java
@@ -27,10 +27,14 @@ import java.io.PrintWriter;
 import java.awt.event.ActionEvent;
 
 public class GestionarVenta extends JPanel {
-	private JTextField txtIdComprar;
-	private JTextField textField;
+	private JTextField txtIdVehiculoVendido;
+	private JTextField txtIdTrabajador;
 	private JTable table;
 	DefaultTableModel modeloTabla = new DefaultTableModel();
+	private static float precio_Compra;
+	private static float precio_Venta;
+	private static String modelo;
+	private JTextField txtIdCliente;
 
 	/**
 	 * Create the panel.
@@ -43,25 +47,8 @@ public class GestionarVenta extends JPanel {
 		JLabel lblInventario = new JLabel("Inventario");
 		lblInventario.setForeground(SystemColor.textHighlight);
 		lblInventario.setFont(new Font("Tahoma", Font.BOLD, 22));
-		lblInventario.setBounds(610, 162, 214, 40);
+		lblInventario.setBounds(596, 40, 214, 40);
 		add(lblInventario);
-
-		JButton btnGenerarTiket = new JButton("Generar tiket ");
-		btnGenerarTiket.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-
-			}
-		});
-		btnGenerarTiket.setForeground(Color.WHITE);
-		btnGenerarTiket.setFont(new Font("Arial", Font.BOLD, 18));
-		btnGenerarTiket.setBackground(Color.BLUE);
-		btnGenerarTiket.setBounds(553, 552, 172, 35);
-		add(btnGenerarTiket);
-
-		txtIdComprar = new JTextField();
-		txtIdComprar.setBounds(596, 468, 129, 19);
-		add(txtIdComprar);
-		txtIdComprar.setColumns(10);
 
 		JLabel lblIdDeVehculo = new JLabel("Id de veh\u00EDculo vendido:");
 		lblIdDeVehculo.setForeground(SystemColor.textHighlight);
@@ -69,20 +56,41 @@ public class GestionarVenta extends JPanel {
 		lblIdDeVehculo.setBounds(395, 462, 183, 27);
 		add(lblIdDeVehculo);
 
-		textField = new JTextField();
-		textField.setBounds(596, 509, 129, 19);
-		add(textField);
-		textField.setColumns(10);
-
 		JLabel lblIdDelTrabajador = new JLabel("Id del trabajador que ha gestionado la venta:");
 		lblIdDelTrabajador.setForeground(SystemColor.textHighlight);
 		lblIdDelTrabajador.setFont(new Font("Arial", Font.BOLD, 16));
 		lblIdDelTrabajador.setBounds(234, 503, 423, 27);
 		add(lblIdDelTrabajador);
 
+		// JTextFields
+		txtIdVehiculoVendido = new JTextField();
+		txtIdVehiculoVendido.setBounds(596, 468, 129, 19);
+		add(txtIdVehiculoVendido);
+		txtIdVehiculoVendido.setColumns(10);
+
+		txtIdTrabajador = new JTextField();
+		txtIdTrabajador.setBounds(596, 509, 129, 19);
+		add(txtIdTrabajador);
+		txtIdTrabajador.setColumns(10);
+
+		JButton btnGenerarTiket = new JButton("Generar tiket ");
+		btnGenerarTiket.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				cargaVenta();
+				insertarVenta();
+
+			}
+		});
+		btnGenerarTiket.setForeground(Color.WHITE);
+		btnGenerarTiket.setFont(new Font("Arial", Font.BOLD, 18));
+		btnGenerarTiket.setBackground(Color.BLUE);
+		btnGenerarTiket.setBounds(520, 589, 172, 35);
+		add(btnGenerarTiket);
+
 		// Tabla
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(56, 213, 1218, 363);
+		scrollPane.setBounds(72, 88, 1218, 363);
 		add(scrollPane);
 
 		table = new JTable();
@@ -91,6 +99,17 @@ public class GestionarVenta extends JPanel {
 		modeloTabla.setColumnIdentifiers(new Object[] { "ID", "Modelo", "Eficiencia_Energetica", "Consumo", "Emisiones",
 				"Precio_Venta", "Precio_Compra", "Caja_Cambios", "Año", "Matricula" });
 		table.setModel(modeloTabla);
+
+		txtIdCliente = new JTextField();
+		txtIdCliente.setColumns(10);
+		txtIdCliente.setBounds(596, 545, 129, 19);
+		add(txtIdCliente);
+
+		JLabel lblIdCliente = new JLabel("Id Cliente:");
+		lblIdCliente.setForeground(SystemColor.textHighlight);
+		lblIdCliente.setFont(new Font("Arial", Font.BOLD, 16));
+		lblIdCliente.setBounds(489, 540, 89, 27);
+		add(lblIdCliente);
 		cargaInventario();
 
 	}
@@ -122,39 +141,65 @@ public class GestionarVenta extends JPanel {
 		}
 	}
 
-	public void ficheroTickets() {
+	public void cargaVenta() {
 		Connection conexion = null;
 		Statement sql = null;
 		ResultSet rs = null;
-		String modelo = "";
-		float precio_compra = 0;
-		float precio_venta = 0;
-
-		java.util.Date fecha = new Date();
-		int dia = fecha.getDay();
-		int mes = fecha.getMonth();
-
+		int idvehiculo = Integer.parseInt(txtIdVehiculoVendido.getText());
 		try {
-			conexion = DriverManager.getConnection("jdbc:mysql://localhost/SotecarsBBDD", "TRABAJO", "TRABAJO");
-			sql = conexion.createStatement();
-			rs = sql.executeQuery(
-					"SELECT ID, Modelo, Eficiencia_Energetica, Consumo, Emisiones, Precio_Venta, Precio_Compra, Caja_Cambios, Año, Matricula from modelos");
+			try {
+				conexion = DriverManager.getConnection("jdbc:mysql://localhost/SotecarsBBDD", "TRABAJO", "TRABAJO");
+				sql = conexion.createStatement();
+				rs = sql.executeQuery(
+						"SELECT Modelo, Precio_Venta, Precio_Compra from modelos where ID = " + idvehiculo);
 
-			while (rs.next()) {
-				modelo = (String) rs.getObject("Modelo");
-				precio_venta = (float) rs.getObject("Precio_Venta");
-				precio_compra = (float) rs.getObject("Precio_Compra");
+				while (rs.next()) {
+					modelo = (String) rs.getObject("modelo");
+					precio_Venta = (Float) rs.getObject("precio_Venta");
+					precio_Compra = (Float) rs.getObject("precio_Compra");
+				}
+
+				conexion.close();
+			} catch (SQLException e) {
+				System.out.println("ERROR AL EJECUTAR LA SENTENCIA SQL");
 			}
-
-			conexion.close();
-		} catch (SQLException e) {
-			System.out.println("ERROR AL EJECUTAR LA SENTENCIA SQL");
+		} finally {
+			System.out.println("Ningun error");
 		}
 	}
+
+	public void insertarVenta() {
+
+		try {
+
+			int idCliente = Integer.parseInt(txtIdCliente.getText());
+			int idVehiculo = Integer.parseInt(txtIdVehiculoVendido.getText());
+			int idTrabajador = Integer.parseInt(txtIdTrabajador.getText());
+
+			String agregar = "INSERT INTO ventas (ID_Cliente, ID_Trabajador, Modelo, Precio_Compra, Precio_Venta) VALUES('"
+					+ idCliente + "', '" + modelo + "', '" + idTrabajador + "', '" + precio_Compra + "', '"
+					+ precio_Venta + "')";
+			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/SotecarsBBDD", "TRABAJO",
+					"TRABAJO");
+
+			Statement consulta = conexion.createStatement();
+
+			consulta.executeUpdate(agregar);
+
+			JOptionPane.showMessageDialog(null, "Venta Agregada Correctamente");
+
+			conexion.close();
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void resetTextFields() {
+
+		txtIdCliente.setText("");
+		txtIdTrabajador.setText("");
+		txtIdVehiculoVendido.setText("");
+	}
+
 }
-
-/**
- * Create the panel.
- */
-
-//Metodos
