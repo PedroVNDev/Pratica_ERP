296015607d065d60ff2608ffb65c4363933fce52
jano3 <jano3@DESKTOP-USBC82G>
2021-01-20 19:26:26 +0100

    asd

--- Practica_ERP/src/paneles/ListadoReparaciones.java
+++ Practica_ERP/src/paneles/ListadoReparaciones.java
@@ -4,12 +4,18 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ListadoReparaciones extends JPanel {
 	private JTable table;
-
+	DefaultTableModel modeloTabla = new DefaultTableModel();
 	/**
 	 * Create the panel.
 	 */
@@ -23,6 +29,13 @@ public class ListadoReparaciones extends JPanel {
 		
 		table = new JTable();
 		scrollPane.setViewportView(table);
+
+		modeloTabla.setColumnIdentifiers(new Object[] { "Empleado ", "Materiales necesarios", "Presupuesto", "Fecha de entrega" });
+		table.setModel(modeloTabla);
+		table.setRowHeight(1, 2);
+
+		modeloTabla.setRowCount(0);
+		cargaReparaciones();
 		
 		JLabel lblSotecars2 = new JLabel("THE POWER OF DREAMS");
 		lblSotecars2.setFont(new Font("Tahoma", Font.PLAIN, 13));
@@ -36,5 +49,30 @@ public class ListadoReparaciones extends JPanel {
 		lblSotecars1.setBounds(627, 86, 228, 52);
 		add(lblSotecars1);
 
+	}
+	
+	public void cargaReparaciones() {
+		Connection conexion = null;
+		Statement sql = null;
+		ResultSet rs = null;
+		try {
+			try {
+				conexion = DriverManager.getConnection("jdbc:mysql://localhost/SotecarsBBDD", "TRABAJO", "TRABAJO");
+				sql = conexion.createStatement();
+				rs = sql.executeQuery(
+						"SELECT id_empleado, descripcion, precio_total, fecha_entrega FROM mantenimiento");
+
+				while (rs.next()) {
+					modeloTabla.addRow(new Object[] { rs.getObject("id_empleado"), rs.getObject("descripcion"), rs.getObject("precio_total"),
+							rs.getObject("fecha_entrega") });
+				}
+
+				conexion.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} finally {
+			System.out.println("Ningun error");
+		}
 	}
 }
