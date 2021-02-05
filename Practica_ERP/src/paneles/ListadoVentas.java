887e8a788e33807d6483816f8794db8681d58b4d
jano3 <jano3@10.10.200.46>
2021-02-05 16:00:17 +0100

    rm gkrgntr

--- Practica_ERP/src/paneles/ListadoVentas.java
+++ Practica_ERP/src/paneles/ListadoVentas.java
@@ -3,8 +3,12 @@ package paneles;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import java.awt.SystemColor;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -13,6 +17,18 @@ import java.sql.Statement;
 
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
 import java.awt.Color;
 
 public class ListadoVentas extends JPanel {
@@ -40,36 +56,42 @@ public class ListadoVentas extends JPanel {
 		lblListadoVentas.setBounds(568, 136, 214, 40);
 		add(lblListadoVentas);
 		
-		modeloTabla.setColumnIdentifiers(new Object[] { "ID", "ID Clientes", "ID Trabajador", "ID Vehiculo", "Modelo",
+		modeloTabla.setColumnIdentifiers(new Object[] { "ID", "ID Clientes", "Nombre Trabajador", "ID Vehiculo", "Modelo",
 				"Precio_Venta", "Precio_Compra" });
 		table.setModel(modeloTabla);
-		cargaInventario();
+		cargarArrayListsVentas();
 
 	}
-	public void cargaInventario() {
+	
+public void cargarArrayListsVentas() {
+		
 		Connection conexion = null;
 		Statement sql = null;
 		ResultSet rs = null;
+		Float precioLoco;
+		
 		try {
 			try {
 				conexion = DriverManager.getConnection("jdbc:mysql://localhost/SotecarsBBDD", "TRABAJO", "TRABAJO");
 				sql = conexion.createStatement();
 				rs = sql.executeQuery(
-						"SELECT ID, ID_Cliente, ID_Trabajador, ID_Vehiculo, Modelo, Precio_Venta, Precio_Compra from ventas");
-
+						"SELECT ventas.ID, ventas.ID_Cliente, ventas.ID_Trabajador, ventas.Fecha, ventas.ID_Vehiculo, ventas.Modelo, ventas.Precio_Venta, ventas.Precio_Compra, trabajadores.Nombre from ventas INNER JOIN Trabajadores on ventas.ID_Trabajador = trabajadores.ID ");
+				
+				String idString, ventasString, ingresosString;
+				
 				while (rs.next()) {
 					modeloTabla.addRow(new Object[] { rs.getObject("ID"), rs.getObject("ID_Cliente"),
-							rs.getObject("ID_Trabajador"), rs.getObject("ID_Vehiculo"), rs.getObject("Modelo"),
+							rs.getObject("trabajadores.Nombre"), rs.getObject("ID_Vehiculo"), rs.getObject("Modelo"),
 							rs.getObject("Precio_Venta"), rs.getObject("Precio_Compra")});
 				}
-
 				conexion.close();
 			} catch (SQLException e) {
-				System.out.println("ERROR AL EJECUTAR LA SENTENCIA SQL");
+				e.printStackTrace();
 			}
 		} finally {
 			System.out.println("Ningun error");
 		}
+		
 	}
 
 }
